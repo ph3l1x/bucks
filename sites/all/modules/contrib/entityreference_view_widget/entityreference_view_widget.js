fec7b2db1f876f4491b5083499fe15757c30db48
ph3l1x <rperry@mindimage.net>
2016-03-09 19:47:26 -0700

    Upgraded server files

--- sites/all/modules/contrib/entityreference_view_widget/entityreference_view_widget.js
+++ sites/all/modules/contrib/entityreference_view_widget/entityreference_view_widget.js
@@ -1,64 +1,57 @@
 (function($) {
-Drupal.behaviors.entityreferenceViewWidget = {
-  attach: function(context, settings) {
-    var checkboxes = '#modal-content input[name="entity_ids[]"]';
-    $('#entityreference_view_widget_select_all').unbind('click').text('Select all').data('unselect', 0).click(function(){
-      if ($(this).data('unselect')) {
-        $(checkboxes).removeAttr('checked');
-        $(this).data('unselect', 0).text('Select all');
-      }
-      else {
-        $(checkboxes).attr('checked', 'checked');
-        $(this).data('unselect', 1).text('Unselect all');
-      }
-      return false;
-    });
-    $('#entityreference-view-widget-modal-submit .button').click(function(){
-      $('#modal-content .error').remove();
-
-      var button = $(this);
-      var selected_amount = $(checkboxes + ':checked').length;
-      var field_name = $('#entityreference-view-widget-field-name').val();
-      var field_frontend_name = field_name.replace(/\_/g, '-');
-      var widget_settings = JSON.parse($('#entityreference-view-widget-' + field_frontend_name + '-settings').val());
-      var offset = $('#' + widget_settings.table_id + ' tbody tr').length;
-      var entity_ids = $(checkboxes).serialize();
-      var query_string = entity_ids + '&field_name=' + field_name + '&langcode=' + widget_settings.langcode + '&target_type=' + widget_settings.target_type + '&cardinality=' + widget_settings.cardinality;
-
-      $('#' + widget_settings.table_id + ' input[type=checkbox]:checked').each(function(){
-        query_string += '&default_entity_ids[' + $(this).data('delta') + ']=' + $(this).val();
-        selected_amount++;
+  Drupal.behaviors.entityreferenceViewWidget = {
+    attach: function(context, settings) {
+      var checkboxes = '#modal-content input.entity-reference-view-widget-select';
+      var selectAllSelector = '#entityreference-view-widget-select-all';
+      $(selectAllSelector).unbind('click').data('unselect', 0).click(function() {
+        if ($(this).data('unselect')) {
+          $(checkboxes).removeAttr('checked');
+          $(this).data('unselect', 0).text(Drupal.t('Select all'));
+        }
+        else {
+          $(checkboxes).attr('checked', 'checked');
+          $(this).data('unselect', 1).text(Drupal.t('Unselect all'));
+        }
+        return false;
       });
 
-      if (widget_settings.cardinality > 0 && widget_settings.cardinality < selected_amount) {
-        $('#modal-content').prepend('<div class="messages error">Please select no more than ' + widget_settings.cardinality + ' values.</div>')
+      if (settings.entityReferenceViewWidget) {
+        var ervwSetttings = settings.entityReferenceViewWidget.settings;
+        if (ervwSetttings.cardinality != -1 || $(checkboxes).length === 0) {
+          $(selectAllSelector).remove();
+        }
+
+        var selector = '#' + ervwSetttings.table_id + ' input[type=checkbox]:checked';
+        var selected_ids = '';
+        $(selector).each(function() {
+          selected_ids += $(this).val() + ';';
+        });
+        if (selected_ids.length > 0) {
+          $('input[name="selected_entity_ids"]').val(selected_ids.substring(0, selected_ids.length - 1)).trigger('change');
+        }
+
+        // We need to pass the settings via an hidden field because Views doesn't
+        // allow us to pass data between ajax requests.
+        if (settings.entityReferenceViewWidget.serialized) {
+          $('input[name="ervw_settings"]').val(settings.entityReferenceViewWidget.serialized);
+        }
+      }
+    }
+  }
+
+  // Create a new ajax command, ervw_draggable that is called to make the rows
+  // of the widget draggable.
+  Drupal.ajax.prototype.commands.ervw_draggable = function(ajax, response, status) {
+    $('#' + response.selector + ' tr').each(function(){
+      var el = $(this);
+      Drupal.tableDrag[response.selector].makeDraggable(el.get(0));
+      el.find('td:last').addClass('tabledrag-hide');
+      if ($.cookie('Drupal.tableDrag.showWeight') == 1) {
+        el.find('.tabledrag-handle').hide();
       }
       else {
-        $.ajax({
-          url: '/?q=entityreference_view_widget/ajax',
-          type: 'POST',
-          dataType: 'html',
-          data: query_string,
-          success: function(data) {
-            data && $('#' + widget_settings.table_id + ' tbody').html(data);
-            $('#' + widget_settings.table_id + ' tbody tr').each(function(){
-              var el = $(this);
-              if (!el.find('.tabledrag-handle').length) {
-                Drupal.tableDrag[widget_settings.table_id].makeDraggable(el.get(0));
-                el.find('td:last').addClass('tabledrag-hide');
-                if ($.cookie('Drupal.tableDrag.showWeight') == 1) {
-                  el.find('.tabledrag-handle').hide();
-                }
-                else {
-                  el.find('td:last').hide();
-                }
-              }
-            });
-            button.hasClass('modal-close') && Drupal.CTools.Modal.dismiss();
-          }
-        });
+        el.find('td:last').hide();
       }
     });
-  }
-}
+  };
 })(jQuery);
