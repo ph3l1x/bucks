811ce059f9dd14e16ac54613dabb92456ba6605e
phelix <rperry@mindimage.net>
2016-06-26 16:01:25 -0600

    bucks updates

--- sites/all/themes/bucks/js/bucks.js
+++ sites/all/themes/bucks/js/bucks.js
@@ -5,7 +5,7 @@
 
         /* Select last word to color for product search title */
 
-        $('#block-views-exp-product-display-nodes-page h2.block-title').html(function(index, theText) {
+        $('#block-views-exp-product-display-nodes-page h2.block-title').html(function (index, theText) {
             var text = theText.split(/[\s-]/),
                 newText = '<span class="last-word">' + text.pop() + '</span>';
             return text.join(' ').concat(' ' + newText);
@@ -17,17 +17,17 @@
 
         /* Modify Category Menu links to redirect to product_search page */
 
-        var categoryApiUrl = 'http://bucks.mindimage.net/api/categories';
-        if($('#block-superfish-1').length > 0) {
+        var categoryApiUrl = 'http://bucks/api/categories';
+        if ($('#block-superfish-1').length > 0) {
             $.ajax(categoryApiUrl, {
                 success: function (data) {
 
-                    $.each(data, function(k, v) {
+                    $.each(data, function (k, v) {
                         var returnTermName = v.name,
                             cleanTermName = returnTermName.replace(/[^\w\s]/gi, ''),
                             cleanTermName = cleanTermName.replace(' ', '-'),
                             tid = v.tid;
-                            matched = $(".block-superfish a[title='" + returnTermName + "']");
+                        matched = $(".block-superfish a[title='" + returnTermName + "']");
                         matched.attr('href', '/product_search/field_category/' + cleanTermName.toLowerCase() + '-' + tid);
                     });
                 },
@@ -40,6 +40,44 @@
         /* Add Additional Images info on product page */
         $('.field-name-field-additional-images .field-label').html('<i class="fa fa-search-plus" aria-hidden="true"></i><span> Additional Images</span>');
         $('.form-type-textfield label').html('Qty:');
+
+        /* Meet Me Mouse Over */
+        $(window).bind('load', function () {
+            var imageWidth = $('.meet-me-picture img').width(),
+                imageHeight = $('.meet-me-picture img').height();
+            $('.meet-me-info').css({
+                'width': imageWidth,
+                'height': imageHeight
+            });
+
+            $('.meetme').mouseenter(function () {
+                $(this).find('.meet-me-info').addClass('meet-me-up');
+            });
+            $('.meetme').mouseleave(function () {
+                $(this).find('.meet-me-info').removeClass('meet-me-up');
+            });
+            console.log('WIDTH: ', imageWidth);
+            console.log("HEIGHT: ", imageHeight);
+
+            $(window).resize(function () {
+                var imageWidth = $('.meet-me-picture img').width(),
+                    imageHeight = $('.meet-me-picture img').height();
+                $('.meet-me-info').css({
+                    'width': imageWidth,
+                    'height': imageHeight
+                });
+
+                $('.meetme').mouseenter(function () {
+                    $(this).find('.meet-me-info').addClass('meet-me-up');
+                });
+                $('.meetme').mouseleave(function () {
+                    $(this).find('.meet-me-info').removeClass('meet-me-up');
+                });
+                console.log('WIDTH: ', imageWidth);
+                console.log("HEIGHT: ", imageHeight);
+            });
+        });
+
     });
 })(jQuery);
 
--- test.php
+++ test.php
@@ -1,3 +1,96 @@
-<?php
-phpinfo();
-?>
+<div class="content-container">
+    <div class="content-content-inner">
+        <div class="meetme col-md-6 col-lg-4 col-xs-12">
+            <div class="meetme-inner">
+                <div class="meet-me-picture"><img alt="" src="/sites/default/files/imce/jeffbuck.jpg"/></div>
+                <div class="meet-me-info">
+                    <div class="meet-me-name">Jeff Buck</div>
+                    <div class="meet-me-title">4x4 Owner</div>
+                    <div class="meet-me-info-bio">Jeff was born in 1969 and grew up in the back room of the Buck’s shop.
+                        By the time he was 14 years old, he began working in the shop installing parts and repairing
+                        4x4’s alongside his brother and sister. In 2003, his brother and sister took on a family
+                        proprietorship, and in 2009 Jeff took over as a sole proprietorship. In his off time away from
+                        running the shop, Jeff enjoys outdoor activities such as hunting, fishing, shooting, 4 wheeling,
+                        and spending all of it with his family.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="meetme col-md-6 col-lg-4 col-xs-12">
+            <div class="meetme-inner">
+                <div class="meet-me-picture"><img alt="" src="/sites/default/files/imce/scott-martin.jpg"/></div>
+                <div class="meet-me-info">
+                    <div class="meet-me-name">Scott Martin</div>
+                    <div class="meet-me-title">Online Samurai</div>
+                    <div class="meet-me-info-bio">Scott Martin is 42 years old from Belvidere, IL. Scott has been a part
+                        of Buck’s 4x4 since 2007, starting in our shop he quickly learned more and more about the
+                        company. Scott now manages our Ecommerce, sales, ordering, shipping and receiving. Scott is very
+                        knowledgeable and a pleasure to have as a part of the Buck’s crew!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="meetme col-md-6 col-lg-4 col-xs-12">
+            <div class="meetme-inner">
+                <div class="meet-me-picture"><img alt="" src="/sites/default/files/imce/Matt_Mares2.jpg"/></div>
+                <div class="meet-me-info">
+                    <div class="meet-me-name">Matt Mares</div>
+                    <div class="meet-me-title">Sales</div>
+                    <div class="meet-me-info-bio">Matt is a life long avid off roader. Today, he runs our sales
+                        department, along with our social media and attends our events and shows. Matt attended UTI in
+                        2008 and graduated with collision repair and refinishing, along with custom paint and fabrication. Matt
+                        is ICAR certified in welding and automotive, and is extremely knowledgeable and great to have on
+                        the Buck’s crew!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="meetme col-md-6 col-lg-4 col-xs-12">
+            <div class="meetme-inner">
+                <div class="meet-me-picture"><img alt="" src="/sites/default/files/imce/ERIC_WESTFALL2.jpg"/></div>
+                <div class="meet-me-info">
+                    <div class="meet-me-name">Eric Westfall</div>
+                    <div class="meet-me-title">Mr. FABulous</div>
+                    <div class="meet-me-info-bio">Eric started with us in 2014 and came on as our fabricator and
+                        installer. Eric has custom fabricated all of his accessories for his own vehicle, including tube
+                        doors, bumpers, and a rear tire gate. Eric attended CWI where he received his welding certification. He
+                        served in the USMC and has over 15 years of experience custom building all kinds of custom
+                        trucks.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="meetme col-md-6 col-lg-4 col-xs-12">
+            <div class="meetme-inner">
+                <div class="meet-me-picture"><img alt="" src="/sites/default/files/imce/Scott-Cherin2.jpg"/></div>
+                <div class="meet-me-info">
+                    <div class="meet-me-name">Scott Cherin</div>
+                    <div class="meet-me-title">Gear General</div>
+                    <div class="meet-me-info-bio">Scott started with us in 2014 and came in as our general technician.
+                        Scott is skilled with gearing and differentials, wiring, engine rebuilds and installation. Scott
+                        graduated UTI in 1987 and then graduated MMI in 2004. He is certified in power sports and has
+                        over 25 years of experience working with all kinds of motorized vehicles.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="meetme col-md-6 col-lg-4 col-xs-12">
+            <div class="meetme-inner">
+                <div class="meet-me-picture"><img alt="" src="/sites/default/files/imce/brad-norman.jpg"/></div>
+                <div class="meet-me-info">
+                    <div class="meet-me-name">Brad Norman</div>
+                    <div class="meet-me-title">Master Installer</div>
+                    <div class="meet-me-info-bio">Brad has been with the Buck’s crew since 2005. ASE certified and our
+                        master installer of everything we sell. Brad has built and customized several Jeeps of his own
+                        that he still owns today, and is a vital part of the Buck’s crew!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
